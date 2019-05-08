6a2ca9fc51af035f56af5b2c9b355565ff549c20
Ciyan97 <ciyan97@live.nl>
2019-05-08 10:50:15 +0200

    Tweede versie van data hashing, getest met cbor

--- Amnesia.Tests/Mining/MiningTest.cs
+++ Amnesia.Tests/Mining/MiningTest.cs
@@ -8,6 +8,7 @@ using Amnesia.Cryptography;
 using Amnesia.Domain.Entity;
 using Amnesia.Domain.Model;
 using NUnit.Framework;
+using PeterO.Cbor;
 
 namespace Amnesia.Tests.Mining
 {
@@ -19,11 +20,11 @@ namespace Amnesia.Tests.Mining
         [Test]
         public void TestHashingOfBlock()
         {
-            var data = new Data(null, 
+            var data = new Data(null,
                 serializer.Serialize("Handtekening"),
                 serializer.Serialize("Dit is een stuk data"));
 
-            var definition = new Definition(data.Hash, null, data.Signature, 
+            var definition = new Definition(data.Hash, null, data.Signature,
                 false, null, data, null, null, null);
 
             var list = new List<Definition> {definition};
@@ -31,12 +32,58 @@ namespace Amnesia.Tests.Mining
             var content = new Content(list, null, null);
 
             var block = new Block(0, serializer.Serialize(content), content);
-           
+
             //aef7925610ce72001910b0bf4674793669cd6fe0fbc706961522ac2fbaeb492b - 0
             //8a9f9dcdf4dbefd8a3108dbb99ca4f05fd93e483fccdf3ee092964038dc481aa - 1
             //69361095198ec77b33d1594303eadd5c478748ad57495a4a484b96e42c565f1c - 2
-            
+
+
             Console.WriteLine("Hash: {0}", block.Calculate256HashString());
+        }
+
+        [Test]
+        public void TestConversion()
+        {
+            var content = new Content(null, null, null);
+
+            var block = new Block(0, serializer.Serialize(content), content);
+
+            var cbor = CBORObject.NewMap()
+                .Add("ContentHash", content.Hash)
+                .Add("PreviousBlockHash", new byte[] {0})
+                .Add("Nonce", 0);
+
+            Console.WriteLine(cbor.ToJSONString());
+            
+            byte[] bytes = cbor.EncodeToBytes();
+            string json = cbor.ToJSONString();
+
+            var sha256bytes = CalculateSha256Hash(bytes);
+            
+            Console.WriteLine(Calculate256HashString(CalculateSha256Hash(bytes)));
+
+            var t = CBORObject.DecodeFromBytes(bytes);
+            
+            Console.WriteLine(t.ToJSONString());
+        }
+        
+        protected byte[] CalculateSha256Hash(byte[] cbor)
+        {   
+            using (SHA256 sha256Hash = SHA256.Create())  
+            {  
+                byte[] bytes = sha256Hash.ComputeHash(cbor);
+                return bytes;
+            }  
+        }
+        
+        public string Calculate256HashString(byte[] sha256)
+        {
+            StringBuilder builder = new StringBuilder();  
+            for (int i = 0; i < sha256.Length; i++)  
+            {  
+                builder.Append(sha256[i].ToString("x2"));  
+            }  
+            return builder.ToString();
         }
     }
 }
