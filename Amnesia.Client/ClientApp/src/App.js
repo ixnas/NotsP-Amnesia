c534f8fb56de673d982c130b8a08876f2c3932a0
meesVDsloot <mees__1995@hotmail.com>
2019-05-20 13:37:13 +0200

    git error

--- Amnesia.Client/ClientApp/src/App.js
+++ Amnesia.Client/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './Layout';
 import { Home } from './components/Pages/Home';
+import {KeyHelper} from './keyHelper.js'
 const NodeRSA = require('node-rsa');
 
 export default class App extends Component {
@@ -15,6 +16,7 @@ export default class App extends Component {
 
     componentDidMount() {
         const key = new NodeRSA({ b: 512 });
+        const key2 = new KeyHelper(key);
         const keySavedInLocal = localStorage.getItem('keySaveState');
         if (!keySavedInLocal) {
             this.keySaver(key)
@@ -25,42 +27,6 @@ export default class App extends Component {
         }
     }
 
-    /**
-     * This function saves the keys into localstorage
-     * param key = the NodeRSA key that gets generated when app.js mounts
-    **/
-
-    keySaver = (key) => {
-        const keyPair = key.generateKeyPair();
-        localStorage.setItem('keySaveState', 'true');
-        localStorage.setItem('pemStringPublic', keyPair.exportKey("public"));
-        localStorage.setItem('pemStringPrivate', keyPair.exportKey("private"));
-
-    }
-
-    /**
-     * This function encrypts a message with the pemString of a private key
-     * param pemStringPrivate = the pem string of the private key saved in localstorage
-     * param message = the message that you want to encrypt
-     **/
-
-    createEncryptedMessagePrivateKey = (pemStringPrivate, message) => {
-        const privateKey = new NodeRSA(pemStringPrivate);
-        const encryptedMessage = privateKey.encryptPrivate(message, 'base64');
-        return encryptedMessage;
-    }
-
-    /**
-     * This function decrypts a message that is encrypted with the corresponding private key
-     * param pemStringPublic = the pem string of the public key saved in localstorage
-     * param message = the message that you want to decrypt
-     **/
-
-    decryptMessagePublicKey = (pemStringPublic, message) => {
-        const publicKey = new NodeRSA(pemStringPublic);
-        const decryptedMessage = publicKey.decryptPublic(message, 'utf8');
-        return decryptedMessage;
-    }
 
     render() {
         return (
